97833236d026963465319c07ff613fcd9645d85d
Hirokazu Chiba <hchiba1@gmail.com>
2022-10-22 17:27:16 +0900

    Add mongo/gene_freq.js

---
+++ mongo/gene_freq.js
@@ -0,0 +1,10 @@
+shellPrint(
+  db.bc2pubtator.aggregate([
+    // { $match: { Species: [{"id": "9606"}]}},
+    { $match: { Species: {$in: [{"id": "9606"}]}}},
+    { $unwind: "$Gene" },
+    { $group: { _id: "$Gene", count: {$sum: 1}}},
+    { $sort: {count: -1} }
+  ]
+  , { allowDiskUse: true }
+  ));
