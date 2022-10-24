a9f3ef5fad24d07d9084b7fbe86899cad3fd8bae
Hirokazu Chiba <hchiba1@gmail.com>
2022-10-24 20:40:17 +0900

    Update mongo/gene_freq.js with alternative match

--- mongo/gene_freq.js
+++ mongo/gene_freq.js
@@ -1,6 +1,7 @@
 shellPrint(
   db.bc2pubtator.aggregate([
     { $match: { Species: {$in: [{"id": "9606"}]}}},
+    // { $match: { "Species.id": "9606"}},
     { $unwind: "$Gene" },
     { $group: { _id: "$Gene", count: {$sum: 1}}},
     { $sort: {count: -1} }
